ea8c10a3e9b3631dca48d39953f0f29ba28e4059
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-04-24 07:20:17 -0500

    sortedIntersection done but needs sort to work

--- Prolog1/Problem4.pl
+++ Prolog1/Problem4.pl
@@ -1,6 +1,13 @@
-share(L, U) :- member(X, L), member(X, U).
-undup(L, [H, J|T]) :- H \= J, member(H, L), undup(L, [J|T]).
+undup([], []).
+undup([X], [X]).
+undup([H, J|T], [K, L|S]) :- H = K, K \= L, H = J, undup([J|T], [K, L|S]).
+undup([H, J|T], [K, L|S]) :- H = K, K \= L, H \= J, undup([J|T], [L|S]).
+undup([H, J|T], [X]) :- H = J, H = X, undup([J|T], [X]).
+
+%sortList([X], [X]).
+%sortList([H,J|T], [K|S]) :- H =< J, H = K, sortList([J|T], S).
 
 sortedUnion(L1,_,U) :- undup(L1, U).
 sortedUnion(_,L2,U) :- undup(L2, U).
-sortedIntersection(L1, L2, U) :- append(L1, L2, Z), undup(Z, U).
+sortedIntersection(L1, L2, U) :- append(L1, L2, Y), sort(Y, Z), undup(Z, U).
+%sortAppend(L1, L2, U) :- append(L1, L2, Z), sort(Z), Z = U.
