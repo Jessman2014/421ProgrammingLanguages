45c20a59c927da5f38fab66c235d2a88d1cf4953
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-04-24 06:56:39 -0500

    trying undup with a share

--- Prolog1/Problem4.pl
+++ Prolog1/Problem4.pl
@@ -1,8 +1,5 @@
-undup([], []).
-undup([X], [X]).
-undup([H, J|T], [K, L|S]) :- H = K, K \= L, H = J, undup([J|T], [K, L|S]).
-undup([H, J|T], [K, L|S]) :- H = K, K \= L, H \= J, undup([J|T], [L|S]).
-undup([H, J|T], [X]) :- H = J, H = X, undup([J|T], [X]).
+share(L, U) :- member(X, L), member(X, U).
+undup(L, [H, J|T]) :- H \= J, member(H, L), undup(L, [J|T]).
 
 sortedUnion(L1,_,U) :- undup(L1, U).
 sortedUnion(_,L2,U) :- undup(L2, U).
