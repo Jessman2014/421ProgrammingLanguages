b548f0f9e4b93405ac568f81a4070d54c3bfd9c0
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-04-25 12:30:53 -0500

    added hailstone changes

--- Prolog1/Problem5.pl
+++ Prolog1/Problem5.pl
@@ -1,2 +1,6 @@
 hailstone(_, [_], 1).
-hailstone(X, [H, J|T], N) :- 
+hailstone(X, [H|T], N) :- 
+
+hailstone(1,[1]) :- !.
+hailstone(N,[N|S]) :- 0 is N mod 2, N1 is N / 2, hailstone(N1,S).
+hailstone(N,[N|S]) :- 1 is N mod 2, N1 is (3 * N) + 1, hailstone(N1, S).
