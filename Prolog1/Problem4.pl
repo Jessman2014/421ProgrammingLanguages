c5eb8d097dd23adbba57b96b026e03b698ec0f45
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-04-25 13:43:21 -0500

    problem4 working but not in sorted order

--- Prolog1/Problem4.pl
+++ Prolog1/Problem4.pl
@@ -7,4 +7,5 @@ undup(L, [H|T]) :- member(H, L), remv(H, L, Z), undup(Z, T).
 sortedUnion(L1,_,U) :- undup(L1, U).
 sortedUnion(_,L2,U) :- undup(L2, U).
 sortedIntersection(L1, L2, U) :- append(L1, L2, Y), undup(Y, U).
-sortedDiff(L1, L2, U) :- undup(L1, W), undup(L2, Y), .
+sortedDiff(L1, [], L1).
+sortedDiff(L1, [H|L2], U) :- undup(L1, W), remv(H, W, Z), sortedDiff(Z, L2, U).
