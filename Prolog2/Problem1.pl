ebdd752e12f0419ed5fa6920fe47219cf3d07e2c
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-05-18 23:25:59 -0500

    got problem 1 working

--- Prolog2/Problem1.pl
+++ Prolog2/Problem1.pl
@@ -1,15 +1,9 @@
-sentence([X1, X2, X3, X4, X5|T], Rest, sentence(nounPhrase(determiner(X1), noun(X2)), verbPhrase(verb(X3),nounPhrase(determiner(X4), noun(X5))))) :- noun_phrase([X1, X2, X3, X4, X5|T],S), verb_phrase(S, Rest), noun_phrase([X4,X5],_),!.
-
-sentence([X1, X2, X3|T], Rest, sentence(nounPhrase(determiner(X1), noun(X2)), verb(X3))) :- noun_phrase([X1, X2, X3|T], S), verb_phrase(S, Rest),!.
-
-sentence(X, X, hasNoStructure).
-
-sentence(S,S0) :- noun_phrase(S,S1),verb_phrase(S1,S0),!.
-noun_phrase(NP,NP0) :- determiner(NP,NP1),noun(NP1,NP0),!.
-verb_phrase(VP,VP0) :- verb(VP,VP1), noun_phrase(VP1,VP0),!.
-verb_phrase(VP,VP0) :- verb(VP,VP0), !.
-determiner([a|Rest],Rest).
-determiner([the|Rest],Rest).
-noun([man|Rest],Rest), noun(man).
-noun([cake|Rest],Rest), noun(cake).
-verb([ate|Rest],Rest), verb(ate).
+sentence(S, Rest, sentence(A, B)) :- noun_phrase(S, S1, A), verb_phrase(S1, Rest, B),!.
+noun_phrase(NP, Rest, noun_phrase(A, B)) :- determiner(NP, NP1, A), noun(NP1, Rest, B),!.
+verb_phrase(VP, Rest, verb_phrase(A, B)) :- verb(VP,VP1, A), noun_phrase(VP1, Rest, B),!.
+verb_phrase(VP, Rest, verb_phrase(A)) :- verb(VP, Rest, A), !.
+determiner([a|Rest], Rest, determiner(a)).
+determiner([the|Rest], Rest, determiner(the)).
+noun([man|Rest], Rest, noun(man)).
+noun([cake|Rest], Rest, noun(cake)).
+verb([ate|Rest], Rest, verb(ate)).
