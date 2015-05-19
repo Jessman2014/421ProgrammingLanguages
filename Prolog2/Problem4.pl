ec1d718e616ea8a54529aaf1c8a56fd8c39dca2f
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-05-18 22:33:47 -0500

    something working for twice paddle

--- Prolog2/Problem4.pl
+++ Prolog2/Problem4.pl
@@ -1,68 +1,49 @@
-crossings([crossing(A,1lr), crossing(B,2lr), crossing(C,3lr), crossing(D,1rl), crossing(E,2rl)]).
-crossing(A,_) :- length(A, 2).
-crossing(B,_) :- length(B, 2).
-crossing(C,_) :- length(C, 2).
-crossing(D,_) :- length(D, 1).
-crossing(E,_) :- length(E, 1).
-paddle([A|_], A).
-member(F, crossings).
-member(G, crossings).
-member(H, crossings).
-member(I, crossings).
+crossing([toRight(_, _), toLeft(_), toRight(_, _), toLeft(_), toRight(_, _)]).
+people([tom, elliot, vic, bill]).
+toLeft(tom).
+toLeft(elliot).
+toRight(elliot, bill).
+toRight(X, Y) :- X \= Y.
 
-isPerson(X, [tom, dick, hery]).
+paddle(X, toLeft(X)).
+paddle(X, toRight(X, _)).
 
-rightToLeft (known things).
-leftToRight (known things).
+%elliotPaddle(X, toLeft(X)).
+%elliotPaddle(X, toRight(X, bill)).
 
-paddledTwice(A) :- crossings(Crossings),
-	isPerson(lkjl, A),
-	isPerson(ljkj, B),
-	isPerson(lkj, C),
-	isPerson(klj, D),
-	A /= B,
-	B /= C,
-	C /= D,
-	A /= C,
-	A /= D,
-	B /= D,
-	leftToRight(dsfs, A),
-	more leftToRight and rightToLefts, -- not sure if needed
-	member(Crossings)
-	print Crossings
-	
-	
+passenger(X, toRight(_, X)).
 
-whoPaddledTwice(Y) :- crossings(Cross), not(member(toRight(tom,_),Cross)),
-						not(member(toRight(elliot,B),Cross)), not(B == bill),
-						paddled(tom,Cross),
-						paddled(elliot,Cross),
-						paddled(bill,Cross),
-						paddled(vic,Cross),
-						twice(Y,Cross),
-						print_crossings(Cross).
-
-crossings([toRight(_,_),
-			toLeft(_),
-			toRight(_,_),
-			toLeft(_),
-			toRight(_,_)]).
-
-twice(X,[H|T]) :- H == toRight(X,_), twice2(X,T).
-twice(X,[H|T]) :- H == toLeft(X), twice2(X,T).
-twice(X,[_|T]) :- twice(X,T).
-
-twice2(X,[H|_]) :- H == toRight(X,_).
-twice2(X,[H|_]) :- H == toLeft(X).
-twice2(X,[_|T]) :- twice2(X,T).
-
-members([tom,elliot,bill,vic]).
-
-toRight(X,Y) :- X \= Y, members(Z), member(X,Z), member(Y,Z).
-toLeft(X) :- members(Z), member(X,Z).
-
-paddled(X,Xs) :- member(toRight(X,_),Xs).
-paddled(X,Xs) :- member(toLeft(X),Xs).
-
-print_crossings([]).
-print_crossings([A|B]) :- write(A), nl, print_crossings(B).
+paddledTwice(A) :- crossing(Crossings),
+	people(People),
+	member(A, People),
+	member(B, People),
+	member(C, People),
+	member(D, People),
+	member(E, Crossings),
+	member(F, Crossings),
+	member(G, Crossings),
+	member(H, Crossings),
+	member(I, Crossings),
+	paddle(A, E),
+	paddle(A, F),
+	paddle(B, G),
+	paddle(C, H),
+	paddle(D, I),
+	E \= F, E \= G, E \= H, E \= I,
+	F \= G, F \= H, F \= I,
+	G \= H, G \= I,
+	H \= I,
+	A \= B,	A \= C, A \= D, 
+	B \= C,	B \= D,
+	C \= D,
+	member(A1, People),
+	member(B1, People),
+	member(C1, People),
+	member(A2, Crossings),
+	member(A2, Crossings),
+	member(C2, Crossings),
+	A2 \= B2, A2 \= C2, B2 \= C2,
+	passenger(A1, A2),
+	passenger(B1, B2),
+	passenger(C1, C2),
+	print(Crossings).
