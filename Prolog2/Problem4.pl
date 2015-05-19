139481477f8bea507b14c39b6355ccae569dac55
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2015-05-19 00:03:48 -0500

    tried to make it work

--- Prolog2/Problem4.pl
+++ Prolog2/Problem4.pl
@@ -1,17 +1,16 @@
 crossing([toRight(_, _), toLeft(_), toRight(_, _), toLeft(_), toRight(_, _)]).
 people([tom, elliot, vic, bill]).
-toLeft(tom).
-toLeft(elliot).
-toRight(elliot, bill).
-toRight(X, Y) :- X \= Y.
 
 paddle(X, toLeft(X)).
-paddle(X, toRight(X, _)).
+paddle(X, toRight(X, Y)).
+elliotPaddle(X, toLeft(X)).
+elliotPaddle(X, toRight(X, bill)).
 
-%elliotPaddle(X, toLeft(X)).
-%elliotPaddle(X, toRight(X, bill)).
-
-passenger(X, toRight(_, X)).
+passenger(X, toRight(Y, X)).
+paddledOrPass(X, toRight(X, Y)).
+paddledOrPass(X, toRight(Y, X)).
+paddledLeft(X, toLeft(X)).
+notSamePerson(toRight(X, Y)) :- X \= Y.
 
 paddledTwice(A) :- crossing(Crossings),
 	people(People),
@@ -36,14 +35,30 @@ paddledTwice(A) :- crossing(Crossings),
 	A \= B,	A \= C, A \= D, 
 	B \= C,	B \= D,
 	C \= D,
+	member(J, Crossings),
+	elliotPaddle(elliot, J),
+	member(toLeft(tom), Crossings),
 	member(A1, People),
 	member(B1, People),
 	member(C1, People),
 	member(A2, Crossings),
-	member(A2, Crossings),
+	member(B2, Crossings),
 	member(C2, Crossings),
 	A2 \= B2, A2 \= C2, B2 \= C2,
+	notSamePerson(A2),
+	notSamePerson(B2),
+	notSamePerson(C2),
 	passenger(A1, A2),
 	passenger(B1, B2),
 	passenger(C1, C2),
+	member(D2, Crossings),
+	member(E2, Crossings),
+	A2 \= D2, B2 \= D2, C2 \= D2, E2 \= D2,
+	A2 \= E2, B2 \= E2, C2 \= E2,
+	paddledLeft(D1, D2),
+	paddledLeft(E1, E2),
+	paddledOrPass(D1, K),
+	paddledOrPass(E1, L),
+	member(K, Crossings),
+	member(L, Crossings),
 	print(Crossings).
